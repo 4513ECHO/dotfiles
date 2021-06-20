ce3fd184002813bce72bf85a3526dd50a418551c
4513ECHO <4513echo@gmail.com>
2021-06-20 17:55:35 +0900

    [fix] replace master with main

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git pull origin master
+git pull origin main
 git submodule init
 git submodule update
-git submodule foreach git pull origin master
+git submodule foreach git pull origin main
