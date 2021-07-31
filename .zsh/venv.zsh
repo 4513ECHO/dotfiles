19d9b40cc27b17b3d5f2a249f6c4df2f6301bcae
4513ECHO <4513echo@gmail.com>
2021-07-31 02:54:16 +0000

    [update] 変数の追加

--- .zsh/venv.zsh
+++ .zsh/venv.zsh
@@ -2,20 +2,29 @@
 
 autoload -Uz add-zsh-hook
 
-DEFAULT_PACKAGES="neovim pep8 pyflakes python-language-server pyls-isort pyls-black"
+export DEFAULT_PACKAGES="pep8:pyflakes"
+export DEFAULT_PACKAGES="${DEFAULT_PACKAGES}:neovim"
+export DEFAULT_PACKAGES="${DEFAULT_PACKAGES}:python-language-server:pyls-isort:pyls-black"
+export VENV_DIR_NAME="venv"
+export REQUESTMENT_TXT="requestments.txt"
 
 function venv-setup ()  {
-  [[ ! -d "venv" ]] && python3 -m venv venv
-  source venv/bin/activate
-  eval "pip install ${DEFAULT_PACKAGES}"
-  [[ -f "requestments.txt" ]] && pip install -r requestment.txt
+  [[ ! -d ${VENV_DIR_NAME} ]] && python3 -m venv ${VENV_DIR_NAME}
+  source ${VENV_DIR_NAME}/bin/activate
+  eval "pip install ${DEFAULT_PACKAGES/:/ }"
+  [[ -f ${REQUESTMENT_TXT} ]] && pip install -r ${REQUESTMENT_TXT} || touch ${REQUESTMENT_TXT}
+}
+
+function venv-clean () {
+  eval "pip uninstall ${DEFAULT_PACKAGES/:/ }"
+  [[ -f ${REQUESTMENT_TXT} ]] && pip uninstall -r ${REQUESTMENT_TXT}
 }
 
 function auto-venv () {
-  if [[ -z "$VIRTUAL_ENV" ]]; then
-    [[ -d venv ]] && source venv/bin/activate
+  if [[ -z "${VIRTUAL_ENV}" ]]; then
+    [[ -d venv ]] && source ${VENV_DIR_NAME}/bin/activate
   else
-    parentdir="$(dirname "$VIRTUAL_ENV")"
+    parentdir="$(dirname "{$VIRTUAL_ENV}")"
     [[ "$PWD"/ != "${parentdir}"/* ]] && deactivate
   fi
 }
