f026ea62dc8aeac3c2869930a21d1ec5e0476a09
4513ECHO <4513echo@gmail.com>
2022-04-17 19:00:54 +0900

    ✨ feat(bin): Update shell scripts

---
+++ bin/gitdiff.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+if command -v delta > /dev/null 2>&1; then
+  delta "$@"
+elif command -v diff-highlight > /dev/null 2>&1; then
+  diff-highlight "$@"
+elif [ -x /usr/share/doc/git/contrib/diff-highlight/diff-highlight ]; then
+  /usr/share/doc/git/contrib/diff-highlight/diff-highlight "$@"
+elif [ -x /usr/share/git/diff-highlight/diff-highlight ]; then
+  /usr/share/git/diff-highlight/diff-highlight "$@"
+else
+  diff "$@"
+fi
