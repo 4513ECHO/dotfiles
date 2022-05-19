4a25d5db6da170555a22d88c16d455034509377b
4513ECHO <4513echo@gmail.com>
2022-05-19 20:43:18 +0900

    feat(vim): Add experimental ddu sources

---
+++ config/nvim/denops/@ddu-sources/quickfix.ts
@@ -0,0 +1,53 @@
+import * as fn from "https://deno.land/x/denops_std@v3.3.1/function/mod.ts";
+import type { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.0/file.ts";
+import type { GatherArguments } from "https://deno.land/x/ddu_vim@v1.6.0/base/source.ts";
+import type { Item } from "https://deno.land/x/ddu_vim@v1.6.0/types.ts";
+import { BaseSource } from "https://deno.land/x/ddu_vim@v1.6.0/types.ts";
+
+interface Params {
+  useLoclist: boolean;
+}
+interface QflistItem {
+  bufnr: number;
+  lnum: number;
+  col: number;
+  text: string;
+}
+
+export class Source extends BaseSource<Params, ActionData> {
+  kind = "file";
+
+  gather(args: GatherArguments<Params>): ReadableStream<Item<ActionData>[]> {
+    return new ReadableStream({
+      async start(controller) {
+        const func = args.sourceParams.useLoclist
+          ? ["getloclist", 0]
+          : ["getqflist"];
+        const items: QflistItem[] = await args.denops.call(...func);
+        controller.enqueue(
+          await Promise.all(
+            items.map(async (i): Promise<Item<ActionData>> => ({
+              word: `${await fn.bufname(
+                args.denops,
+                i.bufnr,
+              )}|${i.lnum} col ${i.col}|${i.text}`,
+              action: {
+                bufNr: i.bufnr,
+                col: i.col,
+                lineNr: i.lnum,
+                text: i.text,
+              },
+            })),
+          ),
+        );
+        controller.close();
+      },
+    });
+  }
+
+  params(): Params {
+    return {
+      useLoclist: false,
+    };
+  }
+}
---
+++ config/nvim/denops/@ddu-sources/scrapbox_pages.ts
@@ -0,0 +1,67 @@
+import type { ActionData } from "https://pax.deno.dev/4513ECHO/ddu-kind-url@0.1.0/denops/@ddu-kinds/url.ts";
+import type {
+  GatherArguments,
+  OnInitArguments,
+} from "https://deno.land/x/ddu_vim@v1.6.0/base/source.ts";
+import type { Item } from "https://deno.land/x/ddu_vim@v1.6.0/types.ts";
+import type {
+  Page,
+  PageList,
+} from "https://pax.deno.dev/scrapbox-jp/types/rest.ts";
+import { BaseSource } from "https://deno.land/x/ddu_vim@v1.6.0/types.ts";
+
+interface Params {
+  project: string;
+  rawTextUrl: boolean;
+}
+
+export class Source extends BaseSource<Params, ActionData> {
+  kind = "url";
+  private pages: Page[] = [];
+
+  async onInit(args: OnInitArguments<Params>): Promise<void> {
+    if (!args.sourceParams.project) {
+      await args.denops.call(
+        "ddu#util#print_error",
+        "Invalid sourceParams: project is empty",
+      );
+      return;
+    }
+    const response = await fetch(
+      `https://scrapbox.io/api/pages/${args.sourceParams.project}`,
+    );
+    if (response.ok) {
+      const result = await response.json() as PageList;
+      this.pages = result.pages;
+    }
+  }
+
+  gather(args: GatherArguments<Params>): ReadableStream<Item<ActionData>[]> {
+    const { pages } = this;
+    return new ReadableStream({
+      start(controller) {
+        const items = pages.map((i): Item<ActionData> => ({
+          word: i.title,
+          action: {
+            url: `https://scrapbox.io/${
+              args.sourceParams.rawTextUrl ? "api/pages/" : ""
+            }${args.sourceParams.project}/${
+              encodeURI(i.title.replaceAll(" ", "_"))
+            }${args.sourceParams.rawTextUrl ? "/text" : ""}`,
+          },
+        }));
+        controller.enqueue(items);
+        controller.close();
+      },
+    });
+  }
+
+  params(): Params {
+    /* NOTE: These values are my personal preferences.
+       I will change these when publishing as plugin. */
+    return {
+      project: "4513echo",
+      rawTextUrl: true,
+    };
+  }
+}
