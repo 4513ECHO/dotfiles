e7612bd4c929ea84153073842097ac56e2255540
4513ECHO <4513echo@gmail.com>
2022-06-22 20:24:47 +0900

    feat(vim): Improve treesitter queries

--- config/nvim/after/queries/vim/highlights.scm
+++ config/nvim/after/queries/vim/highlights.scm
@@ -1,5 +1,6 @@
 [
  "echohl"
+ "echon"
  "filetype"
 ] @keyword
 
@@ -9,3 +10,27 @@
   "plugin"
 ] @keyword)
 (filetype_statement (state) @constant)
+
+; Literal Dictionary
+
+(literal_dictionary
+  key: (literal_key) @field)
+
+[
+  "#{"
+] @punctuation.bracket
+
+; Function Options
+
+((set_item
+   option: (option_name) @_option
+   value: (set_value) @function)
+  (#any-of? @_option
+    "balloonexpr" "bexpr"
+    "diffexpr" "dex"
+    "foldexpr" "fde"
+    "formatexpr" "fex"
+    "includeexpr" "inex"
+    "indentexpr" "inde"
+    "modelineexpr" "mle"
+    "patchexpr" "pex"))
