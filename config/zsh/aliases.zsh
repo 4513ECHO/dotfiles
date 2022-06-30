4ba0aa703c7c8a36dde925db8de99a95e352cd9f
4513ECHO <4513echo@gmail.com>
2022-06-30 20:52:00 +0900

    feat(zsh): Update aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,26 +1,24 @@
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias -- -='cd -'
 
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias -- -="cd -"
-
-alias dot="cd ~/dotfiles"
+alias dot='cd ~/dotfiles'
 
 if type exa > /dev/null; then
-  alias ls="exa -a"
-  alias exa="exa -a"
-  alias tree="exa --tree --group-directories-first --git-ignore --ignore-glob .git"
+  alias ls='exa -a'
+  alias exa='exa -a'
+  alias tree='exa --tree --group-directories-first --git-ignore --ignore-glob .git'
 else
-  alias ls="ls -a --color=auto"
+  alias ls='ls -a --color=auto'
   alias tree="tree -I '\.git|venv|node_modules|__pycache__'"
 fi
 
-alias c="clear"
-alias g="git"
-alias globalip="curl -q globalip.me"
-alias grep="grep --color=auto"
-alias reload="source $ZDOTDIR/.zshrc"
-alias rm="rm -i"
-alias mkdir="mkdir -p"
-alias q=" exit"
-
+alias g='git'
+alias globalip='curl -q globalip.me'
+alias grep='grep --color=auto'
+alias reload="exec $SHELL -l"
+alias cp='cp -i'
+alias rm='rm -i'
+alias mkdir='mkdir -p'
+alias q=' exit'
