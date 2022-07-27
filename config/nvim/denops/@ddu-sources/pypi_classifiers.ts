d4e45bd98103724e2c572a67a8575d7f641c423b
GitHub <noreply@github.com>
2022-07-27 04:22:48 +0000

    chore(deps): update deno dependencies

--- config/nvim/denops/@ddu-sources/pypi_classifiers.ts
+++ config/nvim/denops/@ddu-sources/pypi_classifiers.ts
@@ -2,9 +2,9 @@ import type { ActionData } from "https://pax.deno.dev/Shougo/ddu-kind-word/denop
 import type {
   GatherArguments,
   OnInitArguments,
-} from "https://deno.land/x/ddu_vim@v1.8.5/base/source.ts";
-import type { Item } from "https://deno.land/x/ddu_vim@v1.8.5/types.ts";
-import { BaseSource } from "https://deno.land/x/ddu_vim@v1.8.5/types.ts";
+} from "https://deno.land/x/ddu_vim@v1.8.7/base/source.ts";
+import type { Item } from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
+import { BaseSource } from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
 
 type Params = Record<never, never>;
 
