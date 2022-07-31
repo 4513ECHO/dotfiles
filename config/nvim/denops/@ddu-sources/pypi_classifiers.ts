1fc84374467aa8b3f31b2d980f840a2d95a6fb97
Hibiki <4513echo@gmail.com>
2022-07-31 11:00:59 +0900

    chore(deps): update deno dependencies (#108)
    Co-authored-by: GitHub <noreply@github.com>

--- config/nvim/denops/@ddu-sources/pypi_classifiers.ts
+++ config/nvim/denops/@ddu-sources/pypi_classifiers.ts
@@ -2,11 +2,11 @@ import type { ActionData } from "https://pax.deno.dev/Shougo/ddu-kind-word/denop
 import type {
   GatherArguments,
   OnInitArguments,
-} from "https://deno.land/x/ddu_vim@v1.8.7/base/source.ts";
-import type { Item } from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
-import { BaseSource } from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
-import { readerFromStreamReader } from "https://deno.land/std@0.148.0/streams/conversion.ts";
-import { readLines } from "https://deno.land/std@0.148.0/io/mod.ts";
+} from "https://deno.land/x/ddu_vim@v1.8.8/base/source.ts";
+import type { Item } from "https://deno.land/x/ddu_vim@v1.8.8/types.ts";
+import { BaseSource } from "https://deno.land/x/ddu_vim@v1.8.8/types.ts";
+import { readerFromStreamReader } from "https://deno.land/std@0.150.0/streams/conversion.ts";
+import { readLines } from "https://deno.land/std@0.150.0/io/mod.ts";
 
 type Params = Record<never, never>;
 
--- config/nvim/template/typescript/base-ddu-kind.ts
+++ config/nvim/template/typescript/base-ddu-kind.ts
@@ -1,8 +1,8 @@
-import type { Actions } from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
+import type { Actions } from "https://deno.land/x/ddu_vim@v1.8.8/types.ts";
 import {
   ActionFlags,
   BaseKind,
-} from "https://deno.land/x/ddu_vim@v1.8.7/types.ts";
+} from "https://deno.land/x/ddu_vim@v1.8.8/types.ts";
 
 export interface ActionData {
   placeholder: unknown;
