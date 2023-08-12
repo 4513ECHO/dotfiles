1309ae64f624dbdbd28696ff2e351417c95a279e
4513ECHO <mail@4513echo.dev>
2023-08-12 23:41:56 +0900

    feat(vim): Improve mappings for neovide

--- config/nvim/lua/vimrc/neovide.lua
+++ config/nvim/lua/vimrc/neovide.lua
@@ -2,7 +2,8 @@ if not vim.g.neovide then
   return
 end
 
-vim.opt.guifont = "HackGen35 Console NF:h12"
+local font_base, font_size = "HackGen35 Console NF:h%d", 12
+vim.opt.guifont = font_base:format(font_size)
 vim.opt.linespace = 2
 
 vim.g.neovide_scroll_animation_length = 0.1
@@ -10,14 +11,15 @@ vim.g.neovide_cursor_vfx_mode = "pixiedust"
 vim.g.neovide_cursor_trail_size = 0.5
 vim.g.neovide_cursor_vfx_particle_density = 15
 
+local modifier_base = vim.fn.has "mac" == 1 and "<D-%s>" or "<C-S-%s>"
+---@param key string
+---@return string
+local function modifier(key) return modifier_base:format(key) end
+
 -- paste
-vim.keymap.set(
-  { "i", "c", "t" },
-  vim.fn.has "mac" == 1 and "<D-v>" or "<C-S-v>",
-  function()
-    vim.api.nvim_paste(vim.fn.getreg "+" --[[@as string]], false, -1)
-  end
-)
+vim.keymap.set({ "i", "c", "t" }, modifier "v", function()
+  vim.api.nvim_paste(vim.fn.getreg "+" --[[@as string]], false, -1)
+end)
 
 -- disable mouse selecting
 for _, mod in pairs { "S", "A", "2", "3", "4" } do
@@ -27,3 +29,21 @@ for _, mod in pairs { "S", "A", "2", "3", "4" } do
 end
 vim.keymap.set("", "<LeftDrag>", "<LeftMouse>")
 vim.keymap.set("", "<LeftRelease>", "<Nop>")
+
+-- adjust font size
+---@param size integer
+local function set_font_size(size)
+  font_size = size
+  vim.opt.guifont = font_base:format(font_size)
+  vim.notify(
+    "font size: " .. font_size,
+    vim.log.levels.INFO,
+    { title = "neovide" }
+  )
+end
+vim.keymap.set("n", modifier ";", function() set_font_size(font_size + 1) end)
+vim.keymap.set(
+  "n",
+  modifier "-",
+  function() set_font_size(font_size > 1 and font_size - 1 or 1) end
+)
